d083ce917d6baebfe42ef4e5cf9b937be641b52a
sebastian espeset <sebastian.c.espeset@gmail.com>
2021-06-07 16:47:09 -0500

    fixed CORS issue, now we are cooking

--- api/server.js
+++ api/server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-
-const server = express()
+const express = require('express');
+const cors = require('cors');
+const server = express();
 
 server.use(express.json())
-
+server.use(cors())
 server.get('/',(req,res)=>{
-    res.send(`Hello bubblegum`)
+    res.send({message:`Hello bubblegum`})
 });
 
 module.exports = server
